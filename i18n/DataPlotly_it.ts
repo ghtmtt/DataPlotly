e7dd9b8b017eb92dde8b9d41b47f5683433e7d4b
matteo <matteo.ghetta@gmail.com>
2017-12-09 16:25:54 +0100

    UI it translation updates

--- i18n/DataPlotly_it.ts
+++ i18n/DataPlotly_it.ts
@@ -1,4 +1,5 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="it" sourcelanguage="">
 <context>
     <name>@default</name>
     <message>
