be5eba2c89440f3ea9305e264877c9c4592e704f
Nyall Dawson <nyall.dawson@gmail.com>
2019-07-24 07:53:31 +1000

    Add methods to serialize/deserialize settings to xml, and store/read from project

--- DataPlotly/test/test_plot_settings.py
+++ DataPlotly/test/test_plot_settings.py
@@ -9,12 +9,20 @@
 """
 
 import unittest
+import os
+import tempfile
 from DataPlotly.core.plot_settings import PlotSettings
 from qgis.PyQt.QtXml import QDomDocument, QDomElement
+from qgis.core import QgsProject
 
 
 class DataPlotlySettings(unittest.TestCase):
     """Test plot settings"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.test_read_write_project2_written = False
+        self.test_read_write_project2_read = False
 
     def test_constructor(self):
         """
@@ -59,6 +67,59 @@ class DataPlotlySettings(unittest.TestCase):
         self.assertFalse(res.read_xml(bad_elem))
 
         self.assertTrue(res.read_xml(elem))
+        self.assertEqual(res.plot_type, original.plot_type)
+        self.assertEqual(res.properties, original.properties)
+        self.assertEqual(res.layout, original.layout)
+
+    def test_read_write_project(self):
+        """
+        Test reading and writing to project document
+        """
+        # fake project document
+        doc = QDomDocument("test")
+        doc.appendChild(doc.createElement('qgis'))
+        original = PlotSettings('test', properties={'marker_width': 2, 'marker_size': 5},
+                                layout={'title': 'my plot', 'legend_orientation': 'v'})
+        original.write_to_project(doc)
+
+        res = PlotSettings('gg')
+        res.read_from_project(doc)
+        self.assertEqual(res.plot_type, original.plot_type)
+        self.assertEqual(res.properties, original.properties)
+        self.assertEqual(res.layout, original.layout)
+
+    def test_read_write_project2(self):
+        """
+        Test reading and writing to project, signals based
+        """
+        p = QgsProject()
+        original = PlotSettings('test', properties={'marker_width': 2, 'marker_size': 5},
+                                layout={'title': 'my plot', 'legend_orientation': 'v'})
+
+        self.test_read_write_project2_written = False
+
+        def write(doc):
+            self.test_read_write_project2_written = True
+            original.write_to_project(doc)
+
+        p.writeProject.connect(write)
+
+        path = os.path.join(tempfile.gettempdir(), 'test_dataplotly_project.qgs')
+        self.assertTrue(p.write(path))
+        self.assertTrue(self.test_read_write_project2_written)
+
+        p2 = QgsProject()
+        res = PlotSettings('gg')
+        self.test_read_write_project2_read = False
+
+        def read(doc):
+            res.read_from_project(doc)
+            self.test_read_write_project2_read = True
+
+        p2.readProject.connect(read)
+        self.assertTrue(p2.read(path))
+        self.assertTrue(self.test_read_write_project2_read)
+
         self.assertEqual(res.plot_type, original.plot_type)
         self.assertEqual(res.properties, original.properties)
         self.assertEqual(res.layout, original.layout)
