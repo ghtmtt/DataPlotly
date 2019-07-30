d9f588a37622c89089c7ce06ab0e409759b4371b
Nyall Dawson <nyall.dawson@gmail.com>
2019-07-31 07:46:07 +1000

    Many fixes to setting layout settings in plot dialog

--- DataPlotly/test/test_data_plotly_dialog.py
+++ DataPlotly/test/test_data_plotly_dialog.py
@@ -51,6 +51,69 @@ class DataPlotlyDialogTest(unittest.TestCase):
         # default should be scatter plot
         self.assertEqual(settings.plot_type, 'violin')
 
+    def test_settings_round_trip(self):
+        """
+        Test setting and retrieving settings results in identical results
+        """
+        dialog = DataPlotlyPanelWidget(None, iface=IFACE)
+        settings = dialog.get_settings()
+        # default should be scatter plot
+        self.assertEqual(settings.plot_type, 'scatter')
+
+        # customise settings
+        settings.plot_type = 'violin'
+        settings.properties['name'] = 'my legend title'
+        settings.properties['hover_text'] = 'y'
+        settings.properties['box_orientation'] = 'h'
+        settings.properties['normalization'] = 'probability'
+        settings.properties['box_stat'] = 'sd'
+        settings.properties['box_outliers'] = 'suspectedoutliers'
+        settings.properties['violin_side'] = 'negative'
+        settings.properties['show_mean_line'] = True
+        settings.properties['cumulative'] = True
+        settings.properties['invert_hist'] = 'decreasing'
+
+        settings.layout['legend'] = False
+        settings.layout['legend_orientation'] = 'h'
+        settings.layout['title'] = 'my title'
+        settings.layout['x_title'] = 'my x title'
+        settings.layout['y_title'] = 'my y title'
+        settings.layout['z_title'] = 'my z title'
+        settings.layout['range_slider']['visible'] = True
+        settings.layout['bar_mode'] = 'overlay'
+        settings.layout['x_type'] = 'log'
+        settings.layout['y_type'] = 'category'
+        settings.layout['x_inv'] = 'reversed'
+        settings.layout['y_inv'] = 'reversed'
+        settings.layout['bargaps'] = 0.8
+        settings.layout['additional_info_expression'] = '1+2'
+        settings.layout['bins_check'] = True
+
+        dialog2 = DataPlotlyPanelWidget(None, iface=IFACE)
+        dialog2.set_settings(settings)
+
+        self.assertEqual(dialog2.get_settings().plot_type, settings.plot_type)
+        for k in settings.properties.keys():
+            print(k)
+            self.assertEqual(dialog2.get_settings().properties[k], settings.properties[k])
+        self.assertEqual(dialog2.get_settings().properties, settings.properties)
+        for k in settings.layout.keys():
+            print(k)
+            self.assertEqual(dialog2.get_settings().layout[k], settings.layout[k])
+
+        settings = dialog.get_settings()
+        dialog3 = DataPlotlyPanelWidget(None, iface=IFACE)
+        dialog3.set_settings(settings)
+
+        self.assertEqual(dialog3.get_settings().plot_type, settings.plot_type)
+        for k in settings.properties.keys():
+            print(k)
+            self.assertEqual(dialog3.get_settings().properties[k], settings.properties[k])
+        self.assertEqual(dialog3.get_settings().properties, settings.properties)
+        for k in settings.layout.keys():
+            print(k)
+            self.assertEqual(dialog3.get_settings().layout[k], settings.layout[k])
+
     def test_read_write_project(self):
         """
         Test saving/restoring dialog state in project
