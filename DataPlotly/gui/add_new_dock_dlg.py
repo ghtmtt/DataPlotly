a7f5717e1fec83f3d635a756ffe79f2b2ed150dc
JD LOM <jdaniel.lomenede@gmail.com>
2023-04-10 17:59:26 +0200

    format: pylint

--- DataPlotly/gui/add_new_dock_dlg.py
+++ DataPlotly/gui/add_new_dock_dlg.py
@@ -10,16 +10,17 @@ from DataPlotly.gui.gui_utils import GuiUtils
 
 WIDGET, _ = uic.loadUiType(GuiUtils.get_ui_file_path('add_dock_dlg.ui'))
 
+
 def uuid_suffix(string: str) -> str:
     return f"{string}{uuid.uuid4()}"
 
 
 class DataPlotlyNewDockDialog(QDialog, WIDGET):
-    def __init__(self, dock_widgets = None, parent = None):
+    def __init__(self, dock_widgets=None, parent=None):
         """Dialog to add new dock"""
         super().__init__(parent)
         self.setupUi(self)
-        
+
         self.DockIdInformationLabel.hide()
         validator = DataPlotlyNewDockIdValidator(dock_widgets=dock_widgets)
         self.DockIdLineEdit.setValidator(validator)
@@ -35,7 +36,7 @@ class DataPlotlyNewDockDialog(QDialog, WIDGET):
         style_border = ""
         if not is_valid:
             style_border = "border: 3px solid red"
-            label.show() 
+            label.show()
         else:
             label.hide()
         label.setText(msg)
@@ -45,7 +46,7 @@ class DataPlotlyNewDockDialog(QDialog, WIDGET):
         if not self.mCustomizeGroupBox.isChecked():
             title = self.DockTitleLineEdit.value()
             self.DockIdLineEdit.setValue(uuid_suffix(title))
-    
+
     def get_params(self):
         dock_title = self.DockTitleLineEdit.value()
         dock_id = self.DockIdLineEdit.value()
@@ -55,8 +56,8 @@ class DataPlotlyNewDockDialog(QDialog, WIDGET):
 class DataPlotlyNewDockIdValidator(QValidator):
 
     validationChanged = pyqtSignal(QValidator.State, str)
-    
-    def __init__(self, parent = None, dock_widgets = None):
+
+    def __init__(self, parent=None, dock_widgets=None):
         """Constructor."""
         super().__init__(parent)
         self.dock_widgets = dock_widgets
@@ -73,7 +74,7 @@ class DataPlotlyNewDockIdValidator(QValidator):
         if dock_id in self.dock_widgets:
             state = QValidator.Intermediate
             msg = self.tr(f'DockId {dock_id} is already taken')
-        
+
         if '_' in dock_id:
             state = QValidator.Intermediate
             msg = self.tr('The underscore _ is not allowed')
